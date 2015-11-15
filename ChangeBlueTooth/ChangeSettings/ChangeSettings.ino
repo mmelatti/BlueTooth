e2c390af6e8b824437d0ea8220a8ec806c796ce0
Michael Melatti <melatti.michael@gmail.com>
2015-11-15 10:47:47 -0500

    changeSettings.ino
    Updated the spacing.

--- ChangeBlueTooth/ChangeSettings/ChangeSettings.ino
+++ ChangeBlueTooth/ChangeSettings/ChangeSettings.ino
@@ -6,27 +6,34 @@
 // AT+UART=115200,0,0 （Set transmission rate to 115200 bps, one stop bit and no parity bit）
 // AT+NAME=Smoothie
 // AT+PSWD=0000 Set pairing code to 0000
-
 #include <SoftwareSerial.h>
 #define rxPin 10
 #define txPin 11
+
 SoftwareSerial mySerial(rxPin, txPin); // RX, TX
 char myChar ;
+
 void setup() {
+  
   Serial.begin(9600);   
   Serial.println("AT");
   mySerial.begin(38400);
   mySerial.println("AT");
+  
 }
+
 void loop() {
+  
   while (mySerial.available()) {
     myChar = mySerial.read();
     Serial.print(myChar);
   }
+  
  while (Serial.available()) {
     myChar = Serial.read();
     Serial.print(myChar); //echo
     mySerial.print(myChar);
   }
+  
 }
 
